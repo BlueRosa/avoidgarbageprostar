4810e445f54ad26f5b3b2561dc40e2fc860c1249
Léo Ménigot-Morand <157379757+menigotle@users.noreply.github.com>
2024-12-05 22:49:42 +0100

    Add files via upload

---
+++ AvoidGarbageProStar/Pattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvoidGarbageProStar
+{
+    class Partern
+    {
+        public int a, b, c;
+
+        public Partern(int aa, int bb, int cc)
+        {
+            a = aa;
+            b = bb;
+            c = cc;
+        }
+    }
+}
