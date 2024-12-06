472ef0f4c8ddcb5a3862f679d3eb3b0b85fed843
Legrenzi Maxime <legrenzimaxime38@gmail.com>
2024-12-06 05:06:25 +0100

    echec commit

--- AvoidGarbageProStar/Form1.Designer.cs
+++ AvoidGarbageProStar/Form1.Designer.cs
@@ -28,13 +28,336 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.titleScreen = new System.Windows.Forms.PictureBox();
+            this.gameOverBox = new System.Windows.Forms.PictureBox();
+            this.retryButton = new System.Windows.Forms.PictureBox();
+            this.quitButton = new System.Windows.Forms.PictureBox();
+            this.baril1 = new System.Windows.Forms.PictureBox();
+            this.baril2 = new System.Windows.Forms.PictureBox();
+            this.baril3 = new System.Windows.Forms.PictureBox();
+            this.baril10 = new System.Windows.Forms.PictureBox();
+            this.baril9 = new System.Windows.Forms.PictureBox();
+            this.baril8 = new System.Windows.Forms.PictureBox();
+            this.baril7 = new System.Windows.Forms.PictureBox();
+            this.baril6 = new System.Windows.Forms.PictureBox();
+            this.baril5 = new System.Windows.Forms.PictureBox();
+            this.baril4 = new System.Windows.Forms.PictureBox();
+            this.Start = new System.Windows.Forms.PictureBox();
+            this.poissonBas = new System.Windows.Forms.PictureBox();
+            this.poissonDroite = new System.Windows.Forms.PictureBox();
+            this.poissonGauche = new System.Windows.Forms.PictureBox();
+            this.poissonHaut = new System.Windows.Forms.PictureBox();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titleScreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameOverBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.retryButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quitButton)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Start)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poissonBas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poissonDroite)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poissonGauche)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poissonHaut)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1903, 1037);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // titleScreen
+            // 
+            this.titleScreen.ErrorImage = ((System.Drawing.Image)(resources.GetObject("titleScreen.ErrorImage")));
+            this.titleScreen.Image = ((System.Drawing.Image)(resources.GetObject("titleScreen.Image")));
+            this.titleScreen.Location = new System.Drawing.Point(560, 364);
+            this.titleScreen.Name = "titleScreen";
+            this.titleScreen.Size = new System.Drawing.Size(712, 141);
+            this.titleScreen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.titleScreen.TabIndex = 1;
+            this.titleScreen.TabStop = false;
+            // 
+            // gameOverBox
+            // 
+            this.gameOverBox.Image = ((System.Drawing.Image)(resources.GetObject("gameOverBox.Image")));
+            this.gameOverBox.Location = new System.Drawing.Point(680, 256);
+            this.gameOverBox.Name = "gameOverBox";
+            this.gameOverBox.Size = new System.Drawing.Size(485, 269);
+            this.gameOverBox.TabIndex = 2;
+            this.gameOverBox.TabStop = false;
+            // 
+            // retryButton
+            // 
+            this.retryButton.Image = ((System.Drawing.Image)(resources.GetObject("retryButton.Image")));
+            this.retryButton.Location = new System.Drawing.Point(441, 679);
+            this.retryButton.Name = "retryButton";
+            this.retryButton.Size = new System.Drawing.Size(369, 129);
+            this.retryButton.TabIndex = 3;
+            this.retryButton.TabStop = false;
+            this.retryButton.Click += new System.EventHandler(this.retryButton_Click);
+            // 
+            // quitButton
+            // 
+            this.quitButton.Image = ((System.Drawing.Image)(resources.GetObject("quitButton.Image")));
+            this.quitButton.Location = new System.Drawing.Point(1000, 679);
+            this.quitButton.Name = "quitButton";
+            this.quitButton.Size = new System.Drawing.Size(365, 124);
+            this.quitButton.TabIndex = 4;
+            this.quitButton.TabStop = false;
+            this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+            // 
+            // baril1
+            // 
+            this.baril1.Image = ((System.Drawing.Image)(resources.GetObject("baril1.Image")));
+            this.baril1.Location = new System.Drawing.Point(126, 619);
+            this.baril1.Name = "baril1";
+            this.baril1.Size = new System.Drawing.Size(160, 230);
+            this.baril1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baril1.TabIndex = 5;
+            this.baril1.TabStop = false;
+            // 
+            // baril2
+            // 
+            this.baril2.Image = ((System.Drawing.Image)(resources.GetObject("baril2.Image")));
+            this.baril2.Location = new System.Drawing.Point(283, 429);
+            this.baril2.Name = "baril2";
+            this.baril2.Size = new System.Drawing.Size(160, 230);
+            this.baril2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baril2.TabIndex = 6;
+            this.baril2.TabStop = false;
+            // 
+            // baril3
+            // 
+            this.baril3.Image = ((System.Drawing.Image)(resources.GetObject("baril3.Image")));
+            this.baril3.Location = new System.Drawing.Point(410, 608);
+            this.baril3.Name = "baril3";
+            this.baril3.Size = new System.Drawing.Size(160, 230);
+            this.baril3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baril3.TabIndex = 7;
+            this.baril3.TabStop = false;
+            // 
+            // baril10
+            // 
+            this.baril10.Image = ((System.Drawing.Image)(resources.GetObject("baril10.Image")));
+            this.baril10.Location = new System.Drawing.Point(1278, 443);
+            this.baril10.Name = "baril10";
+            this.baril10.Size = new System.Drawing.Size(160, 230);
+            this.baril10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baril10.TabIndex = 8;
+            this.baril10.TabStop = false;
+            // 
+            // baril9
+            // 
+            this.baril9.Image = ((System.Drawing.Image)(resources.GetObject("baril9.Image")));
+            this.baril9.Location = new System.Drawing.Point(1153, 578);
+            this.baril9.Name = "baril9";
+            this.baril9.Size = new System.Drawing.Size(160, 230);
+            this.baril9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baril9.TabIndex = 9;
+            this.baril9.TabStop = false;
+            // 
+            // baril8
+            // 
+            this.baril8.Image = ((System.Drawing.Image)(resources.GetObject("baril8.Image")));
+            this.baril8.Location = new System.Drawing.Point(1026, 443);
+            this.baril8.Name = "baril8";
+            this.baril8.Size = new System.Drawing.Size(160, 230);
+            this.baril8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baril8.TabIndex = 10;
+            this.baril8.TabStop = false;
+            // 
+            // baril7
+            // 
+            this.baril7.Image = ((System.Drawing.Image)(resources.GetObject("baril7.Image")));
+            this.baril7.Location = new System.Drawing.Point(904, 554);
+            this.baril7.Name = "baril7";
+            this.baril7.Size = new System.Drawing.Size(160, 230);
+            this.baril7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baril7.TabIndex = 11;
+            this.baril7.TabStop = false;
+            // 
+            // baril6
+            // 
+            this.baril6.Image = ((System.Drawing.Image)(resources.GetObject("baril6.Image")));
+            this.baril6.Location = new System.Drawing.Point(779, 462);
+            this.baril6.Name = "baril6";
+            this.baril6.Size = new System.Drawing.Size(160, 230);
+            this.baril6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baril6.TabIndex = 12;
+            this.baril6.TabStop = false;
+            // 
+            // baril5
+            // 
+            this.baril5.Image = ((System.Drawing.Image)(resources.GetObject("baril5.Image")));
+            this.baril5.Location = new System.Drawing.Point(671, 608);
+            this.baril5.Name = "baril5";
+            this.baril5.Size = new System.Drawing.Size(160, 230);
+            this.baril5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baril5.TabIndex = 13;
+            this.baril5.TabStop = false;
+            // 
+            // baril4
+            // 
+            this.baril4.Image = ((System.Drawing.Image)(resources.GetObject("baril4.Image")));
+            this.baril4.Location = new System.Drawing.Point(543, 445);
+            this.baril4.Name = "baril4";
+            this.baril4.Size = new System.Drawing.Size(160, 230);
+            this.baril4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.baril4.TabIndex = 14;
+            this.baril4.TabStop = false;
+            // 
+            // Start
+            // 
+            this.Start.Image = ((System.Drawing.Image)(resources.GetObject("Start.Image")));
+            this.Start.Location = new System.Drawing.Point(776, 814);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(288, 86);
+            this.Start.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Start.TabIndex = 15;
+            this.Start.TabStop = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // poissonBas
+            // 
+            this.poissonBas.Image = ((System.Drawing.Image)(resources.GetObject("poissonBas.Image")));
+            this.poissonBas.Location = new System.Drawing.Point(577, 619);
+            this.poissonBas.Name = "poissonBas";
+            this.poissonBas.Size = new System.Drawing.Size(200, 250);
+            this.poissonBas.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.poissonBas.TabIndex = 16;
+            this.poissonBas.TabStop = false;
+            // 
+            // poissonDroite
+            // 
+            this.poissonDroite.Image = ((System.Drawing.Image)(resources.GetObject("poissonDroite.Image")));
+            this.poissonDroite.Location = new System.Drawing.Point(852, 395);
+            this.poissonDroite.Name = "poissonDroite";
+            this.poissonDroite.Size = new System.Drawing.Size(250, 150);
+            this.poissonDroite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.poissonDroite.TabIndex = 17;
+            this.poissonDroite.TabStop = false;
+            // 
+            // poissonGauche
+            // 
+            this.poissonGauche.Image = ((System.Drawing.Image)(resources.GetObject("poissonGauche.Image")));
+            this.poissonGauche.Location = new System.Drawing.Point(273, 558);
+            this.poissonGauche.Name = "poissonGauche";
+            this.poissonGauche.Size = new System.Drawing.Size(250, 150);
+            this.poissonGauche.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.poissonGauche.TabIndex = 18;
+            this.poissonGauche.TabStop = false;
+            // 
+            // poissonHaut
+            // 
+            this.poissonHaut.Image = ((System.Drawing.Image)(resources.GetObject("poissonHaut.Image")));
+            this.poissonHaut.Location = new System.Drawing.Point(904, 668);
+            this.poissonHaut.Name = "poissonHaut";
+            this.poissonHaut.Size = new System.Drawing.Size(200, 250);
+            this.poissonHaut.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.poissonHaut.TabIndex = 19;
+            this.poissonHaut.TabStop = false;
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.ScoreLabel.Location = new System.Drawing.Point(76, 62);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(195, 49);
+            this.ScoreLabel.TabIndex = 20;
+            this.ScoreLabel.Text = "Score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1884, 1021);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.poissonHaut);
+            this.Controls.Add(this.poissonGauche);
+            this.Controls.Add(this.poissonDroite);
+            this.Controls.Add(this.poissonBas);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.baril4);
+            this.Controls.Add(this.baril5);
+            this.Controls.Add(this.baril6);
+            this.Controls.Add(this.baril7);
+            this.Controls.Add(this.baril8);
+            this.Controls.Add(this.baril9);
+            this.Controls.Add(this.baril10);
+            this.Controls.Add(this.baril3);
+            this.Controls.Add(this.baril2);
+            this.Controls.Add(this.baril1);
+            this.Controls.Add(this.quitButton);
+            this.Controls.Add(this.retryButton);
+            this.Controls.Add(this.gameOverBox);
+            this.Controls.Add(this.titleScreen);
+            this.Controls.Add(this.pictureBox1);
+            this.MinimumSize = new System.Drawing.Size(1900, 1030);
+            this.Name = "Form1";
+            this.Text = "Avoid Garbage Pro Star";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.titleScreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gameOverBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.retryButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quitButton)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.baril4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Start)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poissonBas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poissonDroite)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poissonGauche)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.poissonHaut)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox gameOverBox;
+        private System.Windows.Forms.PictureBox retryButton;
+        private System.Windows.Forms.PictureBox quitButton;
+        private System.Windows.Forms.PictureBox baril1;
+        private System.Windows.Forms.PictureBox baril2;
+        private System.Windows.Forms.PictureBox baril3;
+        private System.Windows.Forms.PictureBox baril10;
+        private System.Windows.Forms.PictureBox baril9;
+        private System.Windows.Forms.PictureBox baril8;
+        private System.Windows.Forms.PictureBox baril7;
+        private System.Windows.Forms.PictureBox baril6;
+        private System.Windows.Forms.PictureBox baril5;
+        private System.Windows.Forms.PictureBox baril4;
+        private System.Windows.Forms.PictureBox titleScreen;
+        private System.Windows.Forms.PictureBox Start;
+        private System.Windows.Forms.PictureBox poissonBas;
+        private System.Windows.Forms.PictureBox poissonDroite;
+        private System.Windows.Forms.PictureBox poissonGauche;
+        private System.Windows.Forms.PictureBox poissonHaut;
+        private System.Windows.Forms.Label ScoreLabel;
     }
 }
 
--- AvoidGarbageProStar/Form1.cs
+++ AvoidGarbageProStar/Form1.cs
@@ -2,19 +2,193 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Schema;
 
-namespace AvoidGarbageProStar
-{
+
+namespace AvoidGarbageProStar {
     public partial class Form1 : Form
     {
+        List<tickles> masda;
+        int[] crapeau = { 960, 540, 0 };
+        bool canard = true;
+        int caunteur = 0;
+        int caunteureur = 0;
+        bool crackers = true;
+        int[][] captus = new int[10][];
         public Form1()
         {
             InitializeComponent();
+            captus[0] = new int[3];captus[1] = new int[3];captus[2] = new int[3];captus[3] = new int[3];captus[4] = new int[3];captus[5] = new int[3];captus[6] = new int[3];captus[7] = new int[3];captus[8] = new int[3];captus[9] = new int[3];
+            captus[0][0] = -3;
+            captus[0][1] = 10;
+            captus[0][2] = 1080;
+            captus[1][0] = -3;
+            captus[1][1] = 10;
+            captus[1][2] = 1080;
+            captus[2][0] = -3;
+            captus[2][1] = 10;
+            captus[2][2] = 1080;
+            captus[3][0] = -3;
+            captus[3][1] = 10;
+            captus[3][2] = 1080;
+            captus[4][0] = -3;
+            captus[4][1] = 10;
+            captus[4][2] = 1080;
+            captus[5][0] = -3;
+            captus[5][1] = 10;
+            captus[5][2] = 1080;
+            captus[6][0] = -3;
+            captus[6][1] = 10;
+            captus[6][2] = 1080;
+            captus[7][0] = -3;
+            captus[7][1] = 10;
+            captus[7][2] = 1080;
+            captus[8][0] = -3;
+            captus[8][1] = 10;
+            captus[8][2] = 1080;
+            captus[9][0] = -3;
+            captus[9][1] = 10;
+            captus[9][2] = 1080;
+            
+
+            //masda=new List<tickles>();
+            while (canard)
+            {
+
+            }
+            /*tristepoisson();
+
+            gamzz();*/
+            
+        }
+        private  gamzz()
+        {
+            while (I5InMyPokets(96) != "MyBadBro" && canard) {/*dommage il se passe rien, maurice modifie ça stp*/ }
+            troufion();
+        }
+        private String I5InMyPokets(int mishail)
+        {if (mishail + 2 >= 3) { cauntair(); return "Troue"; /* it didn't work in booléanne */} return "MyBadBro";}
+        private void boucletteBrothers(String Sonic, int crapoteur200) { if (!string.IsNullOrEmpty(Sonic) && crapoteur200 < 300000000) { globule(crapoteur200); } else { foreach (tickles brandon in masda) { parkourierre(new int[3][],35,brandon); }}}
+        private void sphinctere(int sphincter)
+        {
+            if (sphincter < 10)
+            {
+                tickles riddle = new tickles("crapo");
+                riddle.rigolad = 0;
+                riddle.crapeaud = 0;
+                riddle.miguelcestmichel = 0;
+                riddle.litshis = sphincter;
+                sphinctere(sphincter+1);
+            }
+        }
+        private void globule(int crapoteuse2lastreet) { String troi = "3"; boucletteBrothers(Int32.Parse(troi).ToString(), 300000001);}
+        private void tristepoisson()
+        {
+            //desactiver tous les déchets, ecran de start
+            baril1.Visible = false;
+            baril2.Visible = false;
+            baril3.Visible = false;
+            baril4.Visible = false;
+            baril5.Visible = false;
+            baril6.Visible = false;
+            baril7.Visible = false;
+            baril8.Visible = false;
+            baril9.Visible = false;
+            baril10.Visible = false;
+            poissonBas.Visible = false;
+            poissonHaut.Visible = false;
+            poissonDroite.Visible = false;
+            gameOverBox.Visible = false;
+            retryButton.Visible = false;
+            quitButton.Visible = false;
+        }
+        private void cauntair()
+        {
+            if (caunteur == 30)
+            {
+                caunteur = 0;
+                caunteureur += 1;
+                ScoreLabel.Text = "Score: " + caunteureur + "S";
+            }
+        }
+        private void parkourierre(int[][] parkourierre, int KrAkEnDaNlJardin, tickles brandonnette)
+        {
+            if(brandonnette.litshis%2==1){brandonnette.rigolad=brandonnette.rigolad+21;}else{brandonnette.rigolad=brandonnette.rigolad-21;}
+            if (brandonnette.rigolad - 100 < crapeau[0] && crapeau[0] < brandonnette.rigolad + 100 && brandonnette.crapeaud - 100 < crapeau[1] && crapeau[1] < brandonnette.crapeaud +100)
+            {
+                canard = false;
+            }
+            brandonnette.crapeaud = brandonnette.rigolad * brandonnette.rigolad * captus[brandonnette.singe][0] + brandonnette.rigolad * captus[brandonnette.singe][1] + captus[brandonnette.singe][2]; ;
+        }
+        void troufion()
+        {
+            //affichage du game over et ecran de selection de retry avec score
+        }
+        //fonctions  à cacher dans le code
+        private void brouette2jardin2grenob(int[] hachoir)
+        {
+                if (masda[hachoir[0]].litshis % 2 == 1) { masda[hachoir[0]].rigolad = masda[hachoir[0]].rigolad + 21; } else { masda[hachoir[0]].rigolad = masda[hachoir[0]].rigolad - 21; }
+
+        }
+        private void gonfreaks(int[] batracien)
+        {
+            //modification  de la position vers le haut
+        }
+        private void crapeaud(int[] baracienne)
+        {
+            //modifiaction de la position du poisson vers le bas
+        }
+        private void multipriseikea3fish(int[] miguelcestmichel)
+        {
+            //modification de la position du poisson vers la droite
+        }
+        private void litshit(int[] pizzaronde) {
+            //modification de la position du poisson vers la gauche
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            canard = false;
+        }
+
+        private void quitButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void retryButton_Click(object sender, EventArgs e)
+        {
+            gamzz();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int[] nini = new int[2];
+            nini[0] = 0;
+            nini[1] = 0;
+            brouette2jardin2grenob(nini);
+            int[][] nino = new int[1][];
+            nino[0] = new int[1];
+            nino[0][0] = 33;
+            foreach (tickles brandon in masda) { parkourierre(new int[3][], 35, brandon); }
+            if (e.KeyCode == Keys.Up)
+            {
+
+                crapeau[1] += 28;
+            }
+            else if (e.KeyCode == Keys.Down) { crapeau[1] -= 28; }
+            else if (e.KeyCode == Keys.Left) { crapeau[0] -= 28; }
+            else if(e.KeyCode == Keys.Right) { crapeau[0] += 28; }
+            /*poissonGauche.Location.X = crapeau[0];
+            poissonGauche.Location.Y = crapeau[1];*/
+
         }
     }
-}
+    class tickles{ public string n;public int rigolad;public int singe;public int crapeaud;public int miguelcestmichel;public int litshis;public tickles(string nom) { n = nom; List<tradition>gateau=new List<tradition>();}public void ajouter(DateTime dt, float x, float y, float r) { int r2 = 3; int r3 = r2 + 5; int r4 = r2 + 5; int r5 = r4 + 1; int r6 = r5 + r5; int r7 = r2; int r91 = r3; int r92 = r3; int r8 = r2; int r9 = r2 + 8; int r10 = r3; int r11 = r2; } public void afficher() { Console.WriteLine($"Déplacementsdel'objet{n}:");} private void marcelpagnol(tradition tr) { for (int i = 0; i < 1; i++);Console.WriteLine("Date: 24,32, Position: (12,17), Rotation: 12 degrés"); } class tradition { public DateTime dt; public float x, y, r; public tradition(DateTime date, float xx, float yy, float rr, int rigolad, int v) { dt = date; x = xx; y = yy; r = rr; v = 4; } class schrekblurk { public void tickles() { var g = new tickles("crapeau"); g.ajouter(DateTime.Now, 0, 0, 0);g.ajouter(DateTime.Now.AddSeconds(1),1,1,45);g.ajouter(DateTime.Now.AddSeconds(2),2,2,90);for (int i=0;i<1;i++){g.afficher();}}}}}}
